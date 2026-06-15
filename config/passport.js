fb0d6c8bfc895c67afd9cf0da42aaeef57d9e227
mking <mkinggrafix@gmail.com>
2026-06-14 19:31:57 -0700

    Fix Google Bug

--- config/passport.js
+++ config/passport.js
@@ -6,9 +6,9 @@ const User = require("../models/User");
 passport.use(
   new GoogleStrategy(
     {
-      clientID:     process.env.GOOGLE_CLIENT_ID,
+      clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:  process.env.GOOGLE_CALLBACK_URL, 
+      callbackURL: process.env.GOOGLE_CALLBACK_URL, 
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
