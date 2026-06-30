4deb2f2f5bfbf19ddd5da8b136a239142e525538
mking <mkinggrafix@gmail.com>
2026-06-30 04:25:58 -0700

    Fix Routes Bug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const router   = express.Router();
 const { generateToken } = require("../utils/jwt");
 const {
-  register, login, getMe, updateProfile,
+  register, login, getMe, updateProfile, updatePrivacy,
   deleteAccount, forgotPassword, resetPassword,
 } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
@@ -12,6 +12,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
 router.put("/profile", protect, updateProfile);
+router.put("/privacy", protect, updatePrivacy);
 router.delete("/account", protect, deleteAccount);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
