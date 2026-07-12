06bdc4dccd15137c7d65ae1df230ff244f6dfd82
mking <mkinggrafix@gmail.com>
2026-07-12 14:59:03 -0700

    Fix Route Bug

--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router  = express.Router();
-const { saveScore, getMyScores, getLeaderboard, getTopics } = require("../controllers/scoreController");
+const { saveScore, getMyScores, getLeaderboard, getTopics, getMyRank } = require("../controllers/scoreController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, saveScore);
 router.get("/me", protect, getMyScores);
 router.get("/leaderboard", getLeaderboard);
 router.get("/topics", getTopics);
+router.get("/my-rank", protect, getMyRank);
 
 module.exports = router;
